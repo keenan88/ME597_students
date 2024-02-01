c459a0066512f9c66d2914a7bd0164d32d391edf
keenan88 <lakingsfan08@gmail.com>
2024-02-01 12:06:28 -0500

    removed localization package and copied utilities.py inot motions.py for now. Is poor form, will seperate functionality later

--- ros2_ws/src/movement/movement/motions.py
+++ ros2_ws/src/movement/movement/motions.py
@@ -3,7 +3,7 @@ import rclpy
 
 from rclpy.node import Node
 
-from utilities import Logger, euler_from_quaternion
+#from utilities import Logger, euler_from_quaternion
 from rclpy.qos import QoSProfile
 
 # TODO Part 3: Import message types needed: 
@@ -161,3 +161,96 @@ if __name__=="__main__":
         rclpy.spin(ME)
     except KeyboardInterrupt:
         print("Exiting")
+
+
+from math import atan2, asin, sqrt
+
+M_PI = 3.1415926535
+
+class Logger:
+    def __init__(self, filename, headers=["e", "e_dot", "e_int", "stamp"]):
+        self.filename = filename
+
+        with open(self.filename, 'w') as file:
+            header_str=""
+
+            for header in headers:
+                header_str+=header
+                header_str+=", "
+            
+            header_str+="\n"
+            
+            file.write(header_str)
+
+
+    def log_values(self, values_list):
+
+        with open(self.filename, 'a') as file:
+            vals_str=""
+
+            # TODO Part 5: Write the values from the list to the file
+            ...
+            
+            vals_str+="\n"
+            
+            file.write(vals_str)
+            
+
+    def save_log(self):
+        pass
+
+class FileReader:
+    def __init__(self, filename):
+        
+        self.filename = filename
+        
+        
+    def read_file(self):
+        
+        read_headers=False
+
+        table=[]
+        headers=[]
+        with open(self.filename, 'r') as file:
+            # Skip the header line
+
+            if not read_headers:
+                for line in file:
+                    values=line.strip().split(',')
+
+                    for val in values:
+                        if val=='':
+                            break
+                        headers.append(val.strip())
+
+                    read_headers=True
+                    break
+            
+            next(file)
+            
+            # Read each line and extract values
+            for line in file:
+                values = line.strip().split(',')
+                
+                row=[]                
+                
+                for val in values:
+                    if val=='':
+                        break
+                    row.append(float(val.strip()))
+
+                table.append(row)
+        
+        return headers, table
+
+
+# TODO Part 5: Implement the conversion from Quaternion to Euler Angles
+def euler_from_quaternion(quat):
+    """
+    Convert quaternion (w in last place) to euler roll, pitch, yaw.
+    quat = [x, y, z, w]
+    """
+    ... # just unpack yaw
+    return yaw
+
+
--- ros2_ws/src/movement/setup.py
+++ ros2_ws/src/movement/setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'motions_exe = movement.motions:main'
         ],
     },
 )
